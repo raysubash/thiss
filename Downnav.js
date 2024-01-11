39ccc4437c48ab18b327ed3e61a72937145e4014
subashray <yadavsubash649@gmail.com>
2024-01-11 09:11:45 +0545

    this

---
+++ Downnav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function downnav() {
+  return (
+    <div className='Downnav'>
+      <a id="A" href='About'>Resolved</a>
+      <a id="B" href='/'>Recent</a>
+      <a id="C"href='/'>Top-Voted</a>
+      <a id="D" href='/'>Responsed</a>
+      <a id="E" href='/'>Location</a>
+
+    </div>
+  )
+}
---
+++ Position.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Position() {
+  return (
+    <>
+    <div className='main'>
+
+  <div className='left'>
+iam left
+  </div>
+
+<div className='mid'>
+iam mid
+
+</div>
+
+<div className='right'>
+iam right
+
+</div>
+
+</div>
+</>
+
+
+  )
+}
